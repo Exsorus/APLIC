0dde0c2a44e6f14903ffc01f911a1862d7623345
unknown <sistemas@CHI051PCAL023.alumnos.cst>
2023-10-23 16:15:09 -0300

    Initial commit

---
+++ app/src/main/java/cl/gbrocal/aplic/Splash.java
@@ -0,0 +1,16 @@
+package cl.gbrocal.aplic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
